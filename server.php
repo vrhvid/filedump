b75a16476baf374d35432c27aeb48659aeb462d9
vrhvid <vrhvid@gmail.com>
2025-02-27 23:07:17 +0100

    Popravi spletno stran in strežnik

--- server.php
+++ server.php
@@ -1,30 +1,6 @@
-<?php
+<?php																							//prirejeno po https://www.w3schools.com/php/php_file_upload.asp			
+	$target_file = "filedump/" . $_FILES["uploadedfile"]["name"]
 
-	
-	$target_dir = "../MEDIA/";																								//prirejeno po https://www.w3schools.com/php/php_file_upload.asp
-				
-	$target_file = $target_dir . $file;
-	$uploadOk = 1;
-							
-	$check = getimagesize($_FILES["fot"]["tmp_name"]);
-	if($check == false) {
-		echo 'Datoteka ni fotografija!';
-		$uploadOk = 0;
-	}
+	move_uploaded_file($_FILES["uploadedfile"]["tmp_name"], $target_file);
 
-	if (file_exists($target_file)) {
-		echo 'Datoteka že obstaja!';
-		$uploadOk = 0;
-	}
-
-	if ($uploadOk == 1) {
-		if (move_uploaded_file($_FILES["fot"]["tmp_name"], $target_file)) {																											//konec prirejeno
-			$sql2 = "INSERT INTO letalo (Icao, Registracija, Fotografija, IDModel, Aktivno, Izbrisano) VALUES ('" . $icao . "','" . $registracija . "','" . $file . "','" . $model . "', 1, 0)";
-				
-			mysqli_query($povezava, $sql2);
-			
-			echo 'uspeh';
-			
-		}
-	}
 ?>
