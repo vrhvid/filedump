cfb7bcf541a8618fc9bccb6979b7cf8e1ac9e246
vrhvid <vrhvid@gmail.com>
2025-03-04 20:44:52 +0100

    Popravi server.php v delujoče stanje

--- server.php
+++ server.php
@@ -1,6 +1,4 @@
-<?php																							//prirejeno po https://www.w3schools.com/php/php_file_upload.asp			
-	$target_file = "filedump/" . $_FILES["uploadedfile"]["name"]
-
+<?php																						
+	$target_file = __DIR__ . "/filedump/" . $_FILES["uploadedfile"]["name"];
 	move_uploaded_file($_FILES["uploadedfile"]["tmp_name"], $target_file);
-
 ?>
